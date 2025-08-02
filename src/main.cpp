b0e6e56078bd2916cff3ea85f8e663e62253ce20
Izz4c <izzac.dev@gmail.com>
2025-08-02 11:45:10 +0530

    Add snake eyes and pause function  - Change QUAD rendering to TRIANGLE rendering for snake  - Add eyes for snake that rotate dynamically  - Add a simple pause function (ESC key)

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <vector>
 #include <iostream>
 #include "sound.h"
-const int WIDTH=1000;      // Window Width
-const int HEIGHT=1000;     // Window Height
+#include <math.h>
+const int WIDTH=400;      // Window Width
+const int HEIGHT=400;     // Window Height
 const int CELL_SIZE = 20; // Size of each grid cell
-
+const int EYE_SIZE = 5;   // Control the eye's size
+const double DELAY = 0.08;// Control game speed
 int cols = WIDTH / CELL_SIZE;  // Number of columns in the grid
 int rows = HEIGHT / CELL_SIZE; // Number of rows in the grid
 struct Vec2{
@@ -21,16 +23,21 @@ Vec2 fruit;
 
 int dirX = 1, dirY = 0; // Directions, initially moving to the right (+1 = right, -1 = left)
 double lastTime = 0.0;
-double delay = 0.08;
+double delay = DELAY;
 bool alive=true;
+bool pause=false;
 void drawCell(int x, int y,float r, float g, float b){
-    glColor3f(r,g,b);                             // Set drawing color
-    glBegin(GL_QUADS);                            // Start drawing
-    glVertex2f(x*CELL_SIZE, y*CELL_SIZE);         // Bottom left corner
-    glVertex2f((x+1)*CELL_SIZE, y*CELL_SIZE);     // Bottom right corner
-    glVertex2f((x+1)*CELL_SIZE, (y+1)*CELL_SIZE); // Top left corner
-    glVertex2f(x*CELL_SIZE,(y+1)*CELL_SIZE);      // Top right corner
-    glEnd();                                      // Finish drawing the box
+    glColor3f(r,g,b);                                 // Set drawing color
+    glBegin(GL_TRIANGLES);                            // Begin drawing
+        // First triangle
+        glVertex2i(x*CELL_SIZE, (y+1)*CELL_SIZE);     // Bottom left corner
+        glVertex2i((x+1)*CELL_SIZE, (y+1)*CELL_SIZE); // Bottom right corner
+        glVertex2i((x+1)*CELL_SIZE, y*CELL_SIZE);     // Top right corner
+        // Second triangle
+        glVertex2i(x*CELL_SIZE, (y+1)*CELL_SIZE);     // Bottom left corner
+        glVertex2i((x+1)*CELL_SIZE, y*CELL_SIZE);     // Top right corner
+        glVertex2i(x*CELL_SIZE, y*CELL_SIZE);         // Top left corner
+    glEnd();                                          // Finish drawing
 }
 void drawWhite(){
     glColor3f(1.0f,1.0f,1.0f);
@@ -57,7 +64,6 @@ void spawnFruit() {
 bool checkSelfCollision() {
     const Vec2& head = snake[0];
     for (size_t i = 1; i < snake.size(); ++i) {
-        
         if (snake[i] == head)
             
             return true;
@@ -88,13 +94,45 @@ void update(){
     if(snake[0].y < 0) snake[0].y =rows-1;   // Wrap top ledge -> bottom edge
     
     if (checkSelfCollision()) {
-        std::cout << "Game Over! You collided with yourself.\n";
+        std::cout << "Game Over! You collided with yourself\n"<<std::endl;
         alive = false;
         playGameOverSound();
     }
     
 }
+void drawEye(int x, int y) {
+    glColor3f(0.2f,0.4f,0.2f);
+    glBegin(GL_QUADS);
+        glVertex2i(x, y);                      // Top-left corner of the eye
+        glVertex2i(x + EYE_SIZE, y);           // Top-right corner
+        glVertex2i(x + EYE_SIZE, y + EYE_SIZE);// Bottom-right corner
+        glVertex2i(x, y + EYE_SIZE);           // Bottom-left corner
+    glEnd();
+}
 
+void drawEyesInCorners(int x, int y, int size) {
+    Vec2 EyeL,EyeR;
+
+    if(dirY == 0){ // If moving horizontally
+        EyeL.x = dirX>0?size-(EYE_SIZE+1):1; EyeL.y = dirX>0?1:size-(EYE_SIZE+1);
+        EyeR.x = dirX>0?size-(EYE_SIZE+1):1; EyeR.y = dirX<0?1:size-(EYE_SIZE+1);
+    }
+    if(dirX == 0){ // If moving vertically
+        EyeL.x = dirY<0?1:size-(EYE_SIZE+1); EyeL.y = dirY<0?1:size-(EYE_SIZE+1);
+        EyeR.x = dirY<0?size-(EYE_SIZE+1):1; EyeR.y = dirY<0?1:size-(EYE_SIZE+1);
+    }
+
+    drawEye(x+EyeL.x,y+EyeL.y);
+    drawEye(x+EyeR.x,y+EyeR.y);
+}
+void togglePause(){
+    pause = !pause;
+}
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        togglePause();
+    }
+}
 int main(){
     if(!glfwInit()){
         return -1;
@@ -117,41 +155,46 @@ int main(){
     printf("Window size: %d x %d\n", windowWidth, windowHeight);
     printf("Framebuffer size: %d x %d\n", fbWidth, fbHeight);
     printf("Rows : %d, Columns : %d\n", rows,cols);
-    glfwMakeContextCurrent(window);  // Make the OpenGL context of the created window current
-    glewInit();                    // Initialize GLEW (to allow access to modern OpenGL functions)
+    spawnFruit();                         // Place the first fruit
+    glfwMakeContextCurrent(window);       // Make the OpenGL context of the created window current
+    glewInit();                           // Initialize GLEW (to allow access to modern OpenGL functions)
 
     glOrtho(0, WIDTH, HEIGHT, 0, -1, 1);  // Set orthogonal 2D projection for OpenGL rendering
-    spawnFruit();  // Place the first fruit
+    
     lastTime = glfwGetTime();
     initAudio();  // Initialize audio system
+    
     while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT); // Clear Screen
         
-        // Input handling :
-        if(glfwGetKey(window, GLFW_KEY_UP)==GLFW_PRESS && dirY!=1){ // UP KEY
-            dirX = 0; dirY = -1; // Move Up (-y direction)
-        }
-        if(glfwGetKey(window, GLFW_KEY_DOWN)==GLFW_PRESS && dirY!=-1){ // DOWN KEY
-            dirX = 0; dirY = 1; // Move Down (+y direction)
-        }
-        if(glfwGetKey(window, GLFW_KEY_LEFT)==GLFW_PRESS && dirX!=1){ // LEFT KEY
-            dirX = -1; dirY = 0; // Move Left (-x direction)
-        }
-        if(glfwGetKey(window, GLFW_KEY_RIGHT)==GLFW_PRESS && dirX!=-1){ // RIGHT KEY
-            dirX = 1; dirY = 0; // Move Right (+x direction)
-        }
-        double currentTime = glfwGetTime();
-        if (currentTime - lastTime >= delay) {
-            update();
-            
-            lastTime = currentTime;
-        }
-        drawCell(fruit.x, fruit.y, 1.0f, 1.0f, 0.0f);
-
         for(int i=0;i<snake.size();i++){
             drawCell(snake[i].x, snake[i].y, alive?0.0f:((i%2==0)?0.8f:1.0f), alive?((i%2==0)?1.0f:0.8f):0.0f, 0.0f); //Draw the snake
         }
-        drawWhite();
+        drawEyesInCorners(snake[0].x*CELL_SIZE,snake[0].y*CELL_SIZE, 20);
+        
+        drawCell(fruit.x, fruit.y, 1.0f, 1.0f, 0.0f);
+        // Input handling :
+        glfwSetKeyCallback(window, keyCallback);
+        if(glfwGetKey(window, GLFW_KEY_UP)==GLFW_PRESS && dirY!=1 && !pause){ // UP KEY
+            dirX = 0; dirY = -1; // Move Up (-y direction)
+        }
+        else if(glfwGetKey(window, GLFW_KEY_DOWN)==GLFW_PRESS && dirY!=-1 && !pause){ // DOWN KEY
+            dirX = 0; dirY = 1; // Move Down (+y direction)
+        }
+        else if(glfwGetKey(window, GLFW_KEY_LEFT)==GLFW_PRESS && dirX!=1 && !pause){ // LEFT KEY
+            dirX = -1; dirY = 0; // Move Left (-x direction)
+        }
+        else if(glfwGetKey(window, GLFW_KEY_RIGHT)==GLFW_PRESS && dirX!=-1 && !pause){ // RIGHT KEY
+            dirX = 1; dirY = 0; // Move Right (+x direction)
+        }
+        
+        double currentTime = glfwGetTime();
+        
+        if (currentTime - lastTime >= delay && alive && !pause) {
+            update();
+            lastTime = currentTime;
+        }
+        glEnd();
         glfwSwapBuffers(window);
         glfwPollEvents();
         
